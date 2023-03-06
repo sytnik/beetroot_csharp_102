f808374b928adfce4f9d9376917c9ffecf4d67c1
Oleh Sytnik <osyt@telenor.dk>
2023-03-06 10:58:16 +0200

    hw3 sample added

---
+++ Lesson3/Program.cs
@@ -0,0 +1,15 @@
+﻿Task3();
+
+void Task3()
+{
+    Console.WriteLine("Input x:");
+    int.TryParse(Console.ReadLine(), out var x);
+    Console.WriteLine("Input y:");
+    int.TryParse(Console.ReadLine(), out var y);
+    var sum = 0;
+    if (x == y) sum = x;
+    else
+        for (var i = x; i <= y; i++)
+            sum += i;
+    Console.WriteLine(sum);
+}
