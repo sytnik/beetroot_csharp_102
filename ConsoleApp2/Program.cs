af8a6682d64c15fa776db2541285bc5ee265c7f1
Oleh Sytnik <osyt@telenor.dk>
2023-03-07 10:49:42 +0200

    overload base
    base project

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson4;
+
+// default template with overload
+class Program
+{
+    static void Main()
+    {
+        var x = 1;
+        Console.WriteLine(OverLd(x) + OverLd(x, x));
+    }
+
+    static int OverLd(int x) => x;
+    static int OverLd(int x, int y) => x + y;
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,6 @@
+﻿var x = 1;
+Console.WriteLine(OverLd(x)); // + OverLd(x, x));
+
+// will not compile
+static int OverLd(int x) => x;
+// static int OverLd(int x, int y) => x + y;
