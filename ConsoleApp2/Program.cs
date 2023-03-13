47601e4ed854a660897d9afa92a321ab1adea0d1
Oleh Sytnik <osyt@telenor.dk>
2023-03-13 21:42:54 +0200

    lesson6 code added

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,15 +4,29 @@ class Program
 {
     static void Main()
     {
-        int[] source = {5, 8, 6, 7, 3, 35, 4};
+        int[] source = { 5, 8, 6, 7, 3, 35, 4 };
         int[] sorted1 = Sort(SortAlgorithmType.Selection, OrderBy.Asc, source);
+        // SampleArray();
+        MultidimensionalArray();
+        JaggedArray();
+        TupleTest();
+        // LINQ
+        // [] - array of some type
+        char[] chArr = new[] { 'a', 's', 'd' };
+
+        string str = string.Join(',', source);
+        string str1 = string.Join(",*, ", source);
+        string str2 = string.Join("", chArr);
+
         Console.WriteLine(string.Join(", ", sorted1));
         int[] sorted2 = Sort(SortAlgorithmType.Bubble, OrderBy.Desc, source);
         Console.WriteLine(string.Join(", ", sorted2));
         int[] sorted3 = Sort(SortAlgorithmType.Insertion, OrderBy.Asc, 5, 8, 6, 7, 3, 35, 4);
+        int[] sorted4 = Sort(SortAlgorithmType.Insertion, OrderBy.Asc, new[]{5, 8, 6, 7, 3, 35, 4});
         Console.WriteLine(string.Join(", ", sorted3));
     }
 
+    // gereral Sort method
     static int[] Sort(SortAlgorithmType type, OrderBy order, params int[] source)
     {
         switch (type)
@@ -28,6 +42,7 @@ class Program
         }
     }
 
+    // SelectionSort
     static int[] SelectionSort(OrderBy order, params int[] source)
     {
         int n = source.Length;
@@ -54,6 +69,7 @@ class Program
         return source;
     }
 
+    // BubbleSort
     static int[] BubbleSort(OrderBy order, params int[] source)
     {
         int n = source.Length;
@@ -79,6 +95,7 @@ class Program
         return source;
     }
 
+    // InsertionSort
     static int[] InsertionSort(OrderBy order, params int[] source)
     {
         int n = source.Length;
@@ -123,11 +140,11 @@ class Program
         Desc
     }
 
-    void SampleArray()
+    static void SampleArray()
     {
         //init array
         int[] arr = new int[0];
-        arr = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        arr = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         // index
         int element = arr[5];
         // Index
@@ -136,6 +153,10 @@ class Program
         //Range
         var twoElements = arr[0..2];
         var twoLast = arr[^1..^0];
+        var two = arr[^2..];
+        var two1 = arr[^2..];
+        // todo select l-2 and -3 elems
+        var two2 = arr[^3..2];
         // first elem as new array
         Range range1 = new Range(0, 1);
         var firstButArr = arr[range1];
@@ -144,9 +165,14 @@ class Program
         var firstFour = arr[range2];
     }
 
-    void MultidimensionalArray()
+    static void MultidimensionalArray()
     {
         // 2-dimensional - table
+        // 0.0 0.1
+        // 1.0 1.1
+        // 2.0 2.1
+        // 0 0
+        // todo order 2d array
         int[,] arr = new int[4, 2];
         //3-dimensional - cube
         int[,,] array1 = new int[4, 2, 3];
@@ -156,8 +182,11 @@ class Program
         int[,] array2Da = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
     }
 
-    void JaggedArray()
+    static void JaggedArray()
     {
+        // 0 0 0 0 0
+        // 0 0 0 0
+        // 0 0     
         // jagged array with 3 different rows
         int[][] jaggedArray = new int[3][];
         jaggedArray[0] = new int[5];
@@ -168,8 +197,74 @@ class Program
         {
             new int[] { 1, 3, 5, 7, 9 },
             new int[] { 0, 2, 4, 6 },
-            new int[] { 11, 22 }
+            new int[] { 11, 22 } // 2
         };
+        int[] secondRow = jaggedArray2[1];
+        int val = jaggedArray2[2][1];
 
+        int oneElement = 100;
+        int[] oneElementArray = new[] { 100 };
+        int value = oneElementArray[0]; // 100
+    }
+
+    static void TupleTest()
+    {
+        // tuple (not the valuetuple !) - кортеж
+        // with type
+        Tuple<int, string, string> person = new Tuple<int, string, string>(1, "Steve", "Jobs");
+
+        // create from arguments
+        var person1 = Tuple.Create(1, "Steve", "Jobs");
+        int x = person1.Item1; // returns 1
+        string y = person1.Item2; // returns "Steve"
+        string z = person1.Item3; // returns "Jobs"
+        // person1.Item1 = 3;
+
+        // valuetuple - кортеж значень (.net fw 4.7+ or System.ValueTuple)
+        // long notation
+        ValueTuple<int, string, string> person4 = (1, "Bill", "Gates");
+        int q = person4.Item1; // returns 1
+        string w = person4.Item2; // returns "Bill"
+        string e = person4.Item3; // returns "Gates"
+
+        // new notation
+        var person2 = (1, "Bill", "Gates");
+        //equivalent Tuple
+        var person3 = Tuple.Create(1, "Bill", "Gates");
+
+        // big valuetuple
+        var numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+
+        // named tuple
+        (int Id, string FirstName, string LastName) person5 = (1, "Bill", "Gates");
+        int a = person5.Id; // returns 1
+        string s = person5.FirstName; // returns "Bill"
+        string d = person5.LastName; // returns "Gates"
+        person5.FirstName = "other";
+
+        // var tuple
+        // var - compile type!
+        // simple init
+        var sometuple = (1, "someText", Guid.NewGuid(), 1.34);
+        // var text = sometuple[1]; // no index
+        var text = sometuple.Item2;
+        // named tuple initialization
+        // GUID (globally unique identifier)
+        double someDouble = 1.34;
+        (int id, string textField, Guid someGuid, double doubleVal) tuple2 =
+            (1, "someText", Guid.NewGuid(), someDouble);
+        var val1 = tuple2.someGuid;
+
+        // named tuple (userId, firstName, isActive)
+        // [] of tuples
+        (int userId, string firstName, bool isActive)[] users =
+            new (int userId, string firstName, bool isActive)[]
+            {
+                (1, "John", true),
+                (2, "Jane", true),
+                (3, "Hanz", false)
+            };
+        string secondName = users[1].firstName; //Jane
+        bool thirdUserIsActive = users[2].isActive; //false
     }
 }
