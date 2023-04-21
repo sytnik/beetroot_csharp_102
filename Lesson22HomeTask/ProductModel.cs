65f7b0d74d215c08159dfcd4f5292d3d2b6839f7
Oleh Sytnik <osyt@telenor.dk>
2023-04-21 21:43:21 +0300

    lesson 23 code added

---
+++ Lesson22HomeTask/ProductModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson22HomeTask;
+
+// "name": "Persimmon",
+// "id": 52,
+// "family": "Ebenaceae",
+// "order": "Rosales",
+// "genus": "Diospyros",
+// "nutritions": {
+//     "calories": 81,
+//     "fat": 0.0,
+//     "sugar": 18.0,
+//     "carbohydrates": 18.0,
+//     "protein": 0.0
+// }
+
+public class ProductModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public string Order { get; set; }
+    public string Genus { get; set; }
+    public Nutritions Nutritions { get; set; }
+}
+
+public sealed record Nutritions(int Calories, double Fat, double Sugar, double Carbohydrates, double Protein);
+// public class Nutritions
+// {
+//     public int Calories { get; set; }
+//     public double Fat { get; set; }
+//     public double Sugar { get; set; }
+//     public double Carbohydrates { get; set; }
+//     public double Protein { get; set; }
+// }
--- Lesson22HomeTask/Program.cs
+++ Lesson22HomeTask/Program.cs
@@ -1,5 +1,8 @@
-﻿using Lesson22Lib;
-using Newtonsoft.Json;
+﻿using System.Collections.Concurrent;
+using System.Net.Http.Json;
+using System.Text;
+using Lesson22Lib;
+
 
 namespace Lesson22HomeTask;
 
@@ -7,17 +10,64 @@ internal static class Program
 {
     private static async Task Main()
     {
-        // indentation
+        Console.OutputEncoding = Encoding.UTF8;
+        // init + get the response + deserialize as model
+        try
+        {
+            ProductModel[] result = await new HttpClient()
+                .GetFromJsonAsync<ProductModel[]>("https://www.fruityvice.com/api/fruit/all");
+            Console.ReadLine();
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
         
-        
-        var persons = 
-            JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
-        // TextJsonSerialization.SerializePersons(persons);
-        // await TextJsonSerialization.SerializePersonsAsync(persons);
-        // var persons2 = TextJsonSerialization.DeserializePersons().ToList();
-        // persons.SerializePersons();
-        // persons.SerializePersonsToXml();
-        // List<Person> persons2 = XmlSerialization.DeserializePersonsFromXml().ToList();
-        NpoiTest.PublishExcel();
+        // init httpclient
+        HttpClient httpClient = new HttpClient();
+        // // get response
+        // var resp = await httpClient.GetAsync("https://www.fruityvice.com/api/fruit/all");
+        // // check for success
+        // resp.EnsureSuccessStatusCode();
+        // // get the content as string
+        // string content = await resp.Content.ReadAsStringAsync();
+        // // get the content as class objects
+        // ProductModel[] products = await resp.Content.ReadFromJsonAsync<ProductModel[]>();
+    }
+
+    private static void PLinqTest()
+    {
+        // Створіть приклад колекції
+        List<int> numbers = new List<int> {23, 12, 45, 67, 89, 30, 5, 14};
+        // Використовуйте PLINQ для упорядкування колекції паралельно
+        var orderedNumbers = numbers.AsParallel().OrderBy(n => n).ToList();
+        // Надрукуйте відсортовану колекцію
+        Console.WriteLine("Упорядкована колекція:");
+        foreach (var number in orderedNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    private static void TplTest()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        // Створення колекції цілих чисел
+        List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        // Створення безпечної для потоків колекції для зберігання квадратів чисел
+        ConcurrentBag<int> squares = new ConcurrentBag<int>();
+        // Виконання паралельних операцій на кожному елементі колекції numbers
+        Parallel.ForEach(numbers, number =>
+        {
+            Console.WriteLine($"Обробка числа {number} на потоці {Task.CurrentId}");
+            int square = number * number;
+            squares.Add(square);
+        });
+        Console.WriteLine("Квадрати чисел:");
+        foreach (int square in squares)
+        {
+            Console.WriteLine(square);
+        }
     }
 }
